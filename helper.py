2e508e639403fbdb4f0710a68007b0e8c7ce7cfb
Sois Smalburg <fransoissmalburg@gmail.com>
2025-06-03 13:35:41 +0200

    wijzigingen Les 9 in helper file

--- helper.py
+++ helper.py
@@ -1,3 +1,10 @@
+def fooi_pp(bedrag, personen):
+    try:
+        bedrag_pp = bedrag/personen
+    except:
+        bedrag_pp = "??"
+    return f"Het bedrag per persoon is {bedrag_pp} euro"
+
 def decoreer(tekst=""):
     lengte = len(tekst) + 4
     print()
@@ -5,3 +12,9 @@ def decoreer(tekst=""):
     print(f"* {tekst} *")
     print(lengte * "*")
     print()
+
+if __name__ == "__main__":
+    b = int(input("Welk bedrag zit er in de fooienpot? "))
+    p = int(input("Over hoeveel mensen moet de pot verdeeld worden? "))
+    resultaat = fooi_pp(b, p)
+    decoreer(resultaat)
